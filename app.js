8f195c39cfe621421c62d9a6697ea8a342ef2d26
Willem C. van Daalen <wvandaalen@outlook.com>
2014-03-04 15:40:53 -0500

    Comments code. Adds strict mode. Removes jQuery dependency and extraneous images.

--- app.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  var canvas = $("<canvas width='" + 1875 +
-                 "' height='" +950 + "'></canvas>");
-  $('body').append(canvas);
-
-  // `canvas.get(0)` unwraps the jQuery'd DOM element;
-  new Asteroids.Game(1875, 950).start(canvas.get(0));
-});
